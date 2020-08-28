6fbc1ad85f676f351c59a0d622fac90f83456e86
Arron Cao <qqcao.cs@gmail.com>
2020-08-28 09:17:29 -0400

    better cached dataset naming

--- examples/nlp-energy/run_energy_squad.py
+++ examples/nlp-energy/run_energy_squad.py
@@ -414,7 +414,7 @@ def load_and_cache_examples(args, tokenizer, evaluate=False, output_examples=Fal
     cached_features_file = os.path.join(
         input_dir,
         "cached_{}_{}_{}".format(
-            "dev" if evaluate else "train",
+            args.predict_file if evaluate else args.train_file,
             list(filter(None, args.model_name_or_path.split("/"))).pop(),
             str(args.max_seq_length),
         ),
